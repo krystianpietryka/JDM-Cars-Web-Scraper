9cbb16ea9c5b9453d1366241725dd61cdf143695
Krystian Pietryka <krystian.pietryka@ggsitc.com>
2023-04-27 20:17:22 +0200

    more parameters extracted

--- main.py
+++ main.py
@@ -27,9 +27,13 @@ for car_offer in car_offers:
 
         make_model_p= car_offer.find("p", class_="make-model")
         make_model_a = make_model_p.find("a",{"class":"vehicle-url-link"})
-        make_model = make_model_a.contents[0].strip()
         vehicle_url = str(homepage_url) + str(make_model_a.get("href"))
-        car_model = make_model_a.contents[0].strip()
+        # indexing from 1 if the first word is a year
+        car_model = make_model_a.contents[0].split()
+        if car_model[0].isnumeric():
+            car_model = ' '.join(car_model[1:])
+        else:
+            car_model = ' '.join(car_model)
 
         mileage_td = car_offer.find("td", class_ = "basic-spec-col basic-spec-col-bordered mileage")
         mileage = mileage_td.find("p", class_='val').contents[0].strip()
@@ -39,6 +43,12 @@ for car_offer in car_offers:
         engine = engine_td.find("p", class_='val').contents[0].strip()
         location_td = car_offer.find("p", class_ = "val stock-area")
         location = location_td.find("span").contents[0].strip()
+        original_price = car_offer.find("p", class_ = "original-vehicle-price").contents[0].strip().replace(',', '')
+        current_price =  car_offer.find("span", class_ = "price").contents[0].strip().replace('$', '').replace(",", '')
+        discount = car_offer.find("p", class_ = "save-rate").contents[0].strip()
+        total_price_p = car_offer.find("p", class_ = "total-price")
+        total_price = total_price_p.find("span", class_=None).contents[0].strip().replace('$', '').replace(",", '')
+        shipping_price = int(total_price) - int(current_price)
         
         print('\n\n')
         print(vehicle_url)
@@ -47,6 +57,11 @@ for car_offer in car_offers:
         print(year)
         print(engine)
         print(location)
+        print(original_price)
+        print(current_price)
+        print(discount)
+        print(total_price)
+        print(shipping_price)
     except Exception as e:
         print(e)
         pass
