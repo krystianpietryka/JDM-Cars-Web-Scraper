c53e29497525801453a3a70fbfd51ed6154bc049
Krystian Pietryka <krystian.pietryka@ggsitc.com>
2023-05-02 23:48:19 +0200

    removed modelCode = 0

--- excel_statistics.py
+++ excel_statistics.py
@@ -19,5 +19,6 @@ def save_to_excel(writer, dataframe, sheet_name):
 def average_price_per_model_code(dataframe):
     avg_price_per_model = dataframe.groupby(["Model Code"]).aggregate({'Model': 'max', "Model Code":'max', "Total Price USD":'mean',  "Color" : 'count'})
     avg_price_per_model = avg_price_per_model.astype({"Total Price USD": int})
-    avg_price_per_model.rename(columns = {'Color':'Model Count', 'Total Price USD':'Average Price USD'}, inplace=True)
-    return avg_price_per_model
+    avg_price_per_model.rename(columns = {'Color':'Model Count', 'Total Price USD':'Average Price USD', 'Model Code':'ModelCode'}, inplace=True)
+    avg_price_per_model_final = avg_price_per_model.where(avg_price_per_model.ModelCode != 0)
+    return avg_price_per_model_final
